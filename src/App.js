12ebe3b5740c4addd968d958b4ddfff34f3b3038
Kevin Bluer <kevin@bluer.com>
2021-04-23 21:45:14 -0500

    loading images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useRef, useState } from "react";
 
+import axios from 'axios';
 import styled from 'styled-components';
 import {SketchField, Tools} from 'react-sketch';
+
+import shades from './Assets/shades.png';
+
+const IPFS = require('ipfs-mini');
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
 const StyledSketchField = styled(SketchField)`
   border: 1px #fff dashed;
@@ -27,27 +32,47 @@ function App() {
 
   const _sketch = useRef();
 
-  const load = () => {
-    // alert('beeple')
-    debugger
-    _sketch.current.addImg('https://ipfsgateway.makersplace.com/ipfs/QmZ15eQX8FPjfrtdX3QYbrhZxJpbLpvDpsgb2p3VEH8Bqq')
+  const addShades = () => {
+    _sketch.current.addImg(shades)
+  }
+
+  const load = async () => {    
+
+    const url = "https://bafybeibfwqxsjs3xzn4txvxllghxevj6gmn3m6jrszpnyda6n5qdt3lidy.ipfs.dweb.link/image.gif";
+
+    const response = await axios.get(url, { responseType: 'blob' }); 
+    const reader = new FileReader();
+    reader.readAsDataURL(response.data); 
+    reader.onload = () => {
+      _sketch.current.addImg(reader.result)
+      // _sketch.current.setBackgroundFromDataUrl(reader.result, {
+      //   stretched: true,
+      //   stretchedX: true,
+      //   stretchedY: true,
+      //   originX: "left",
+      //   originY: "top",
+      // }); 
+    }
   }
 
   const mint = () => {
-    alert(JSON.stringify(_sketch.current.toJSON()))
+    // const metadata = JSON.stringify(_sketch.current.toJSON());
+    const img = _sketch.current.toDataURL()
+    ipfs.add(img).then(console.log).catch(console.log);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
+          <Button onClick={() => addShades() }>shades</Button>
           <Button onClick={() => load() }>load</Button>
           <Button onClick={() => mint() }>mint</Button>
         </div>
         <StyledSketchField 
-          width='1024px' 
+          width='768px' 
           height='768px' 
-          tool={Tools.Pencil} 
+          tool={Tools.Select} 
           lineColor='black'
           lineWidth={3}
           ref={(c) => { _sketch.current = c }}
