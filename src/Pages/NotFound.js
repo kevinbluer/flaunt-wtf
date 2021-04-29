ac0da3eee9e7f75621098004af55c24597bbfa5f
Kevin Bluer <kevin@bluer.com>
2021-04-29 18:47:44 -0500

    page not found

---
+++ src/Pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = ({contract}) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>not found 😕</h1>
+      </header>
+    </div>
+  )
+};
+
+export default NotFound;
